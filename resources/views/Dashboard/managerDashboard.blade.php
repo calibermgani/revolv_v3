bff43f81eba721961350ae8379f9a3dbbb946f3f
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2024-09-17 17:35:08 +0530

    replace CE with AR in Reports Grid and status dropdown

--- resources/views/Dashboard/managerDashboard.blade.php
+++ resources/views/Dashboard/managerDashboard.blade.php
@@ -656,9 +656,13 @@
                     console.error('encodedclientname is undefined or empty');
                     return;
                 }
+                if(subProjectName == '--') {
+                    decodeSubProjectName = subProjectName;
+                } else {
+                    decodeSubProjectName = btoa(subProjectName);
+                }
 
-                var url = baseUrl + 'projects_assigned/' + btoa(clientName) + '/' + btoa(
-                    subProjectName);
+                var url = baseUrl + 'projects_assigned/' + btoa(clientName) + '/' + decodeSubProjectName;
                 var params = {
                     parent: getUrlVars()["parent"],
                     child: getUrlVars()["child"],
@@ -773,8 +777,12 @@
                 // window.location.href = baseUrl + 'projects_hold/' + btoa(clientName) + '/' + btoa(
                 //         subProjectName) + "?parent=" +
                 //     getUrlVars()["parent"] + "&child=" + getUrlVars()["child"];
-                    var url = baseUrl + 'projects_hold/' + btoa(clientName) + '/' + btoa(
-                    subProjectName);
+                if(subProjectName == '--') {
+                    decodeSubProjectName = subProjectName;
+                } else {
+                    decodeSubProjectName = btoa(subProjectName);
+                }
+                    var url = baseUrl + 'projects_hold/' + btoa(clientName) + '/' + decodeSubProjectName;
                     var params = {
                         parent: getUrlVars()["parent"],
                         child: getUrlVars()["child"],
