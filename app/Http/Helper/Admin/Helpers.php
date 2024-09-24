df92d74ae7d224aa8480a5dfc3e53f95064dc7db
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2024-09-24 13:46:36 +0530

    AR Emp List Permission

--- app/Http/Helper/Admin/Helpers.php
+++ app/Http/Helper/Admin/Helpers.php
@@ -656,7 +656,7 @@ class Helpers
 			'token' => '1a32e71a46317b9cc6feb7388238c95d'
 		];
 		$client = new Client();
-		$response = $client->request('POST',  config("constants.PRO_CODE_URL") . '/api/v1_users/get_emp_list', [
+		$response = $client->request('POST',  config("constants.PRO_CODE_URL") . '/api/v1_users/get_ar_emp_list', [
 			'json' => $payload
 		]);
 		if ($response->getStatusCode() == 200) {
