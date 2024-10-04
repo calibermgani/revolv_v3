5361c8b8e42ff3449efe90aa0ebe784f8625653e
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2024-10-04 15:31:11 +0530

    inventory list grid in manager dashboard

--- resources/views/Dashboard/managerDashboard.blade.php
+++ resources/views/Dashboard/managerDashboard.blade.php
@@ -1049,6 +1049,7 @@
                                 scrollY: 180,
                                 "initComplete": function(settings, json) {
                                     $('body').find('.dataTables_scrollBody').addClass("scrollbar");
+                                    $('body').find('.dataTables_scrollBody').css("width",'99%','important');
                                     
                                 },        
                             })
