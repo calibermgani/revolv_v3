ba83545afc803f7bd67a15dd813afe8aee6f634e
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2024-10-24 14:59:41 +0530

    AM4644

--- app/Http/Controllers/ProductionController.php
+++ app/Http/Controllers/ProductionController.php
@@ -289,7 +289,7 @@ class ProductionController extends Controller
                         if (!empty($existingCallerChartsWorkLogsInprocess)) {
                             $assignedProjectDetails = $assignedProjectDetails->orderByRaw('FIELD(id, ' . implode(',', $existingCallerChartsWorkLogsInprocess) . ') DESC');
                         }
-                       $assignedProjectDetails = $assignedProjectDetails->orderBy('id', 'ASC')->paginate(50);dd($assignedProjectDetails,$existingCallerChartsWorkLogs,$loginEmpId);
+                       $assignedProjectDetails = $assignedProjectDetails->orderBy('id', 'ASC')->paginate(50);
                        $assignedCount = $modelClass::whereIn('chart_status',['CE_Assigned','CE_Inprocess'])->where('CE_emp_id',$loginEmpId)->count();
                        $completedCount = $modelClass::where('chart_status','CE_Completed')->where('CE_emp_id',$loginEmpId)->whereBetween('updated_at',[$startDate,$endDate])->count();
                        $pendingCount = $modelClass::where('chart_status','CE_Pending')->where('CE_emp_id',$loginEmpId)->whereBetween('updated_at',[$startDate,$endDate])->count();
