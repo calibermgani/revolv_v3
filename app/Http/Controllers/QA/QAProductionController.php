f070c18ab08d1c8a5e38f00615ace802976ae55b
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2024-10-25 15:57:42 +0530

    export in tabs

--- app/Http/Controllers/QA/QAProductionController.php
+++ app/Http/Controllers/QA/QAProductionController.php
@@ -146,7 +146,7 @@ class QAProductionController extends Controller
                     $column_names = DB::select("DESCRIBE $table_name");
                     $columns = array_column($column_names, 'Field');
                     $columnsToExclude = ['ce_hold_reason','qa_hold_reason','qa_work_status','QA_rework_comments','QA_required_sampling','QA_rework_comments','coder_rework_reason','coder_error_count','qa_error_count','tl_error_count','tl_comments','QA_followup_date','CE_status_code','CE_sub_status_code','CE_followup_date',
-                    'cpt_trends','icd_trends','modifiers','qa_cpt_trends','qa_icd_trends','qa_modifiers',
+                    'cpt_trends','icd_trends','modifiers','annex_coder_trends','annex_qa_trends','qa_cpt_trends','qa_icd_trends','qa_modifiers',
                     'updated_at', 'created_at', 'deleted_at'];
                     $columnsHeader = array_filter($columns, function ($column) use ($columnsToExclude) {
                         return !in_array($column, $columnsToExclude);
