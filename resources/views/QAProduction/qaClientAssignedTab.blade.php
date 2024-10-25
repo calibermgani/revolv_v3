ea7dc923ab34c6b629437e086617eb64f8a205cf
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2024-10-25 14:17:31 +0530

    export implementation in qa assign tab

--- resources/views/QAProduction/qaClientAssignedTab.blade.php
+++ resources/views/QAProduction/qaClientAssignedTab.blade.php
@@ -3904,59 +3904,59 @@ use Carbon\Carbon;
                                 "parent"] +
                             "&child=" + getUrlVars()["child"];
                     })
-                    $(document).on('click', '#assign_export', function(e) {
-                        var formData = $('#formSearch').serialize();
-                        var chartStatus = "CE_Completed";
-                        var recordStatusVal = "Assigned";
-                        formData += '&chart_status=' + chartStatus;
-                        formData += '&clientName=' + clientName;
-                        formData += '&subProjectName=' + subProjectName;
-                        formData += '&recordStatusVal=' + recordStatusVal;
-                         $.ajaxSetup({
-                            headers: {
-                                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr(
-                                    'content')
-                            }
-                        });
-                        KTApp.block('#export_div', {
-                            overlayColor: '#000000',
-                            state: 'danger',
-                            opacity: 0.1,
-                            message: 'Fetching...',
-                        });
-                        $.ajax({
-                                url: "{{ url('qa_production/quality_export') }}",
-                                method: 'POST',
-                                data: formData,
-                                xhrFields: {
-                                    responseType: 'blob'  // This is crucial for downloading Excel
-                                },
-                                success: function(response, status, xhr) {  // Correct order of parameters
-                                    var filename = "";
-                                    var disposition = xhr.getResponseHeader('Content-Disposition');
-                                    if (disposition && disposition.indexOf('attachment') !== -1) {
-                                        var matches = /filename[^;=\n]*=([^;\n]*)/.exec(disposition);                            
-                                        if (matches != null && matches[1]) {
-                                            // Trim any extra spaces or quotes around the filename
-                                            filename = matches[1].trim().replace(/^"|"$/g, '');
-                                        }
-                                    }
-
-                                    var blob = new Blob([response], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
-                                    var link = document.createElement('a');
-                                    link.href = window.URL.createObjectURL(blob);
-                                    link.download = filename || 'export.xlsx';
-                                    document.body.appendChild(link);
-                                    link.click();
-                                    document.body.removeChild(link);
-                                    KTApp.unblock('#export_div');
-                                },
-                                error: function(response) {
-                                    console.log('Error generating Excel file', response);
+                        $(document).on('click', '#assign_export', function(e) {
+                            var formData = $('#formSearch').serialize();
+                            var chartStatus = "CE_Completed";
+                            var recordStatusVal = "Assigned";
+                            formData += '&chart_status=' + chartStatus;
+                            formData += '&clientName=' + clientName;
+                            formData += '&subProjectName=' + subProjectName;
+                            formData += '&recordStatusVal=' + recordStatusVal;
+                            $.ajaxSetup({
+                                headers: {
+                                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr(
+                                        'content')
                                 }
-                        });
+                            });
+                            KTApp.block('#export_div', {
+                                overlayColor: '#000000',
+                                state: 'danger',
+                                opacity: 0.1,
+                                message: 'Fetching...',
+                            });
+                            $.ajax({
+                                    url: "{{ url('qa_production/quality_export') }}",
+                                    method: 'POST',
+                                    data: formData,
+                                    xhrFields: {
+                                        responseType: 'blob'  // This is crucial for downloading Excel
+                                    },
+                                    success: function(response, status, xhr) {  // Correct order of parameters
+                                        var filename = "";
+                                        var disposition = xhr.getResponseHeader('Content-Disposition');
+                                        if (disposition && disposition.indexOf('attachment') !== -1) {
+                                            var matches = /filename[^;=\n]*=([^;\n]*)/.exec(disposition);                            
+                                            if (matches != null && matches[1]) {
+                                                // Trim any extra spaces or quotes around the filename
+                                                filename = matches[1].trim().replace(/^"|"$/g, '');
+                                            }
+                                        }
 
-            });
+                                        var blob = new Blob([response], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+                                        var link = document.createElement('a');
+                                        link.href = window.URL.createObjectURL(blob);
+                                        link.download = filename || 'export.xlsx';
+                                        document.body.appendChild(link);
+                                        link.click();
+                                        document.body.removeChild(link);
+                                        KTApp.unblock('#export_div');
+                                    },
+                                    error: function(response) {
+                                        console.log('Error generating Excel file', response);
+                                    }
+                            });
+
+                        });
         })
 
         function updateTime() {
