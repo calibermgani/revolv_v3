3ac89d4ac85205e1c3f6f08f70b5ab5cd8c2ca90
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2024-10-25 17:21:26 +0530

    export in tabs

--- resources/views/productions/clientAssignedTab.blade.php
+++ resources/views/productions/clientAssignedTab.blade.php
@@ -231,7 +231,7 @@ use Carbon\Carbon;
                                                         <div class="col-md-10">
                                                             @if ($options == null)
                                                                 @if ($inputType != 'date_range')
-                                                                    {!! Form::$inputType($columnName. '[]',isset($searchData) && !empty($searchData) ? $searchData[$columnName] : null, [
+                                                                    {!! Form::$inputType($columnName,isset($searchData) && !empty($searchData) ? $searchData[$columnName] : null, [
                                                                         'class' => 'form-control ' . $columnName . ' white-smoke pop-non-edt-val',
                                                                         'autocomplete' => 'none',
                                                                         'style' => 'cursor:pointer',
