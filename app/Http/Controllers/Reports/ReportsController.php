a6a3de141ea924d802802e627581adf42a9db88d
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2024-10-29 14:33:41 +0530

    loader in report index popup submit

--- app/Http/Controllers/Reports/ReportsController.php
+++ app/Http/Controllers/Reports/ReportsController.php
@@ -181,7 +181,7 @@ class ReportsController extends Controller
                  
                     $body_info .= '<tr>';
                     foreach ($checkedValues as $header) {
-                        $data = isset($row->{$header}) && !empty($row->{$header}) ? $row->{$header} : "--";   $type = gettype($row->{$header});dd($type,$row->{$header});
+                        $data = isset($row->{$header}) && !empty($row->{$header}) ? $row->{$header} : "--";   $type = gettype($row->{'dos'});dd($row->{$header},$type ,$data);
                         if ($header == 'QA_status_code') {
                             if ($data != '--') {
                                 $data = Helpers::qaStatusById($data)['status_code'];
@@ -316,6 +316,9 @@ class ReportsController extends Controller
                         if ($header === 'aging_range') {
                             $data = $agingRange;
                         }
+                        if ($type === 'Date') {
+                            $data = date('m/d/y',strtotime($data));
+                       }
                         $body_info .= '<td class="wrap-text">' . $data . '</td>';
                     }
                     $body_info .= '</tr>';
