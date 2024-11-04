6b72b9f8fa6de1b919a942784a5b6bf154c44e5d
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2024-11-04 12:56:03 +0530

    c

--- app/Http/Controllers/ProductionController.php
+++ app/Http/Controllers/ProductionController.php
@@ -586,11 +586,12 @@ class ProductionController extends Controller
                         if (is_numeric($value) || is_bool($value)) {
                             $query->where($key, $value);  // Exact match for numeric/boolean
                         } elseif ($this->isDate($value)) {  // Check if it's a date
-                            dd($value);
+                         
                             $query->whereDate($key, '=', $value);  // Use `whereDate` for exact date match
                         } elseif (strpos($value, '$') !== false || strpos($value, '.') !== false) {
                             $query->where($key, $value); // For amounts (e.g., "$214.44"), adjust as needed
                         } else {
+                            dd($value);
                             $query->where($key, 'like', '%' . $value . '%'); // Use 'like' for partial text matches
                         }
                     }
