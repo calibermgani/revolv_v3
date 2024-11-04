8f8c63963f76b003ff7a319617cb8977a7dd5fcd
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2024-11-04 12:13:25 +0530

    resources/views/productions/clientCompletedTab.blade.php

--- resources/views/productions/clientCompletedTab.blade.php
+++ resources/views/productions/clientCompletedTab.blade.php
@@ -210,7 +210,7 @@ use Carbon\Carbon;
                                                             }
                                             }
                                          $clientName = App\Http\Helper\Admin\Helpers::encodeAndDecodeID($data->project_id, 'encode');
-                                         $subProjectName = $data->sub_project_id != null ? App\Http\Helper\Admin\Helpers::encodeAndDecodeID($data->sub_project_id, 'encode') : '--';
+                                         $subProjectName = $data->sub_project_id != null ? App\Http\Helper\Admin\Helpers::encodeAndDecodeID($data->sub_project_id, 'encode') : '--';dd($searchData,$projectColSearchFields);
                                             @endphp
                                              {!! Form::open([
                                                 'url' =>
