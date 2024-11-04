a620710c5f4333e59ef5bff1e9301e7b5b39bade
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2024-11-04 12:43:20 +0530

    c

--- app/Http/Controllers/ProductionController.php
+++ app/Http/Controllers/ProductionController.php
@@ -593,6 +593,7 @@ class ProductionController extends Controller
                     }
                 }
                $startDate = Carbon::now()->subDays(30)->startOfDay()->toDateTimeString();$endDate = Carbon::now()->endOfDay()->toDateTimeString(); $yesterDayDate = Carbon::yesterday()->endOfDay()->toDateTimeString();$unAssignedCount = 0;
+               dd($request->all(),$startDate,$endDate);
                $completedProjectDetails = collect();$duplicateCount = 0;$assignedCount=0; $completedCount = 0; $pendingCount = 0;   $holdCount =0;$reworkCount = 0;$subProjectId = $subProjectName == '--' ?  NULL : $decodedPracticeName;
                if ($loginEmpId && ($loginEmpId == "Admin" || strpos($empDesignation, 'Manager') !== false || strpos($empDesignation, 'VP') !== false || strpos($empDesignation, 'Leader') !== false || strpos($empDesignation, 'Team Lead') !== false || strpos($empDesignation, 'CEO') !== false || strpos($empDesignation, 'Vice') !== false)) {
                    if (class_exists($modelClass)) {
