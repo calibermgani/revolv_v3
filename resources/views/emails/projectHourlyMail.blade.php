73aa719bf0631db55332e12bf4ce42d916f1a004
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2024-11-07 12:42:45 +0530

    hourly mail

--- resources/views/emails/projectHourlyMail.blade.php
+++ resources/views/emails/projectHourlyMail.blade.php
@@ -37,7 +37,7 @@
             <p>Hello Team, </p>
         </h4> --}}
 
-        <p>Hello Team - Find below the Resolv  Hourly report for {{$yesterday->format('m/d/Y')}}</p>
+        <p>Hello Team - Find below the Resolv  Hourly report for</p>
         {{-- <p>Please find below the daily update for the production inventory : 06/07/2024</p> --}}
        
         <table class="table" border="1" style="border-collapse: collapse">
