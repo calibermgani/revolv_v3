96d9fb0cef90608731d4c721df40a511c32d5ac4
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2024-11-07 12:59:38 +0530

    hourly mail

--- resources/views/emails/projectHourlyMail.blade.php
+++ resources/views/emails/projectHourlyMail.blade.php
@@ -36,8 +36,11 @@
         {{-- <h4>
             <p>Hello Team, </p>
         </h4> --}}
-
-        <p>Hello Team - Find below the Resolv  Hourly report for {{$today->format('m/d/Y h:i A')}}</p>
+    @php
+    $today = \Carbon\Carbon::now()->setTime(17, 0); // 17:00 is 5 PM in 24-hour format
+    $formattedDate = $today->format('m/d/Y h:i A');
+    @endphp
+        <p>Hello Team - Find below the Resolv  Hourly report for {{$formattedDate->format('m/d/Y h:i A')}}</p>
         {{-- <p>Please find below the daily update for the production inventory : 06/07/2024</p> --}}
        
         <table class="table" border="1" style="border-collapse: collapse">
