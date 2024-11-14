56a5971016b7e4cda42f04441624918eccf3764b
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2024-11-14 11:55:30 +0530

    check qa search

--- app/Http/Controllers/QA/QAProductionController.php
+++ app/Http/Controllers/QA/QAProductionController.php
@@ -156,7 +156,7 @@ class QAProductionController extends Controller
                 $modelName = Str::studly($table_name);
                 $modelClass = "App\\Models\\" . $modelName;
                 $query = $modelClass::query();
-                $searchData = [];dd($request->all());
+                $searchData = [];dd($request->all(),$empDesignation);
                 if($request['_token'] != null) {
                      foreach ($request->except('_token', 'parent', 'child') as $key => $value) {
                         $searchData[$key] = $value;
