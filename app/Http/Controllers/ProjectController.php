11fce813bb62d4222b7ad206c3f7401fc68b8a8f
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2024-11-19 14:55:47 +0530

    hourly mail

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -885,7 +885,7 @@ public function projectHourlyMail()
         Log::info('Executing Project Hourly Mail logic.');
 
         $toMailId = ["vijayalaxmi@caliberfocus.com"];
-        $ccMailId = ["vijayalaxmi@caliberfocus.com"];
+        $ccMailId = ["mgani@caliberfocus.com"];
         $mailHeader = "Resolv Project Hourly Report";
         $projects = collect($this->getProjects());
 
