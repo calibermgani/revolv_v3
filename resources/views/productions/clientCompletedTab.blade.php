aefc558ef5b21e40e6adc1cdd9a5ae8ebf49152f
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2024-11-20 15:33:55 +0530

    filter paginatin

--- resources/views/productions/clientCompletedTab.blade.php
+++ resources/views/productions/clientCompletedTab.blade.php
@@ -211,6 +211,7 @@ use Carbon\Carbon;
                                             }
                                          $clientName = App\Http\Helper\Admin\Helpers::encodeAndDecodeID($data->project_id, 'encode');
                                          $subProjectName = $data->sub_project_id != null ? App\Http\Helper\Admin\Helpers::encodeAndDecodeID($data->sub_project_id, 'encode') : '--';
+                                         dd($projectColSearchFields,$data);
                                             @endphp
                                              {!! Form::open([
                                                 'url' =>
@@ -224,7 +225,7 @@ use Carbon\Carbon;
                                                 'enctype' => 'multipart/form-data',
                                             ]) !!}
                                             @csrf
-                                           
+                                          
                                         @if ($count % 4 == 0)
                                                 <div class="row mr-0 ml-0 mt-5">
                                                     @endif
