ea92db334c1baa231200853817e7f36aa6d03bf5
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2024-11-25 12:52:14 +0530

    project utilization web

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -1085,7 +1085,7 @@ class ProjectController extends Controller
     public function projectWorkWeb(Request $request)
     {
         try {
-            dd($requst->all());
+            dd($request->all());
             $yesterday = Carbon::yesterday();
             if ($yesterday->isSaturday()) {
                 $yesterday = $yesterday->subDay(1); // Friday
