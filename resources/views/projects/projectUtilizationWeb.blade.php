5886b5b60315bdf5fffa92658994cd8dfc4f5e8c
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2024-11-25 13:43:45 +0530

    project utilization web

--- resources/views/projects/projectUtilizationWeb.blade.php
+++ resources/views/projects/projectUtilizationWeb.blade.php
@@ -3,8 +3,6 @@
     <div class="card card-custom custom-card">
         <div class="card card-custom custom-top-border">
             <div class="card-body mr-8 ml-12" id="filter_section">
-
-
                 {!! Form::open([
                     'url' => url('projects/project_work_web'),
                     'class' => 'form',
@@ -14,22 +12,16 @@
                 @csrf
 
                 <div class="row mr-0 ml-0">
-
                     <div class="col-md-3">
                         <div class="form-group row row_mar_bm">
-
                             <div class="col-md-10">
-
                                 {!! Form::date('request_date', isset($yesterday) && !empty($yesterday) ? $yesterday : null, [
                                     'class' => 'form-control white-smoke pop-non-edt-val',
                                     'autocomplete' => 'none',
                                     'style' => 'cursor:pointer',
                                     'rows' => 3,
                                 ]) !!}
-
                             </div>
-
-
                         </div>
                     </div>
                     <div class="col-md-3">
@@ -45,15 +37,9 @@
                                     </span>
                                 </button>
                             </div>
-
-
                         </div>
                     </div>
-
                 </div>
-
-
-
             </div>
             <div class="card-body py-0 px-7">
                 <div class="table-responsive pb-2">
@@ -62,35 +48,15 @@
                         border="1" style="border-collapse: collapse">
                         <thead>
                             <tr>
-                                <th>
-                                    Project</th>
-                                {{-- <th style="text-align: left;padding: 5px;">Chats</th> --}}
-                                <th
-                                    style="text-align: center;padding: 5px;background-color:#2f75b5;color:#ffffff;font-weight: 100;border-color:black;">
-                                    Inventory Uploaded</th>
-                                <th
-                                    style="text-align: center;padding: 5px;background-color:#2f75b5;color:#ffffff;font-weight: 100;border-color:black;">
-                                    Total Users - AR</th>
-                                <th
-                                    style="text-align: center;padding: 5px;background-color:#2f75b5;color:#ffffff;font-weight: 100;border-color:black;">
-                                    Logged Resolv - AR</th>
-                                <th
-                                    style="text-align: center;padding: 5px;background-color:#2f75b5;color:#ffffff;font-weight: 100;border-color:black;">
-                                    Production Users - AR</th>
-                                <th
-                                    style="text-align: center;padding: 5px;background-color:#2f75b5;color:#ffffff;font-weight: 100;border-color:black;">
-                                    AR</th>
-                                {{-- <th style="text-align: center;padding: 5px;background-color:#2f75b5;color:#ffffff;font-weight: 100;border-color:black;">Total QA</th> --}}
-                                <th
-                                    style="text-align: center;padding: 5px;background-color:#2f75b5;color:#ffffff;font-weight: 100;border-color:black;">
-                                    Logged Resolv - QA</th>
-                                <th
-                                    style="text-align: center;padding: 5px;background-color:#2f75b5;color:#ffffff;font-weight: 100;border-color:black;">
-                                    Production - QA</th>
-                                <th
-                                    style="text-align: center;padding: 5px;background-color:#2f75b5;color:#ffffff;font-weight: 100;border-color:black;">
-                                    QA</th>
-                                {{-- <th style="text-align: left;padding: 5px;">Balance</th> --}}
+                                <th>Project</th>
+                                <th>Inventory Uploaded</th>
+                                <th>Total Users - AR</th>
+                                <th>Logged Resolv - AR</th>
+                                <th>Production Users - AR</th>
+                                <th>AR</th>
+                                <th>Logged Resolv - QA</th>
+                                <th>Production - QA</th>
+                                <th>QA</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -98,20 +64,15 @@
                             @if (isset($mailBody) && count($mailBody) > 0)
                                 @foreach ($mailBody as $data)
                                     <tr>
-                                        <td style="text-align: center;padding: 5px;">{{ $data['project'] }}</td>
-                                        <td style="text-align: center;padding: 5px;">
-                                            {{ $data['Chats'] == 0 ? 'No' : 'Yes' }}</td>
-                                        <td style="text-align: center;padding: 5px;">{{ $data['total_ar'] }}</td>
-                                        <td style="text-align: center;padding: 5px;">{{ $data['logged_resolv_ar'] }}</td>
-                                        <td style="text-align: center;padding: 5px;">{{ $data['prodcution_ar'] }}</td>
-                                        <td style="text-align: center;padding: 5px;">
-                                            {{ $data['Coder'] == 0 ? 'No Activity' : $data['Coder'] }}</td>
-                                        {{-- <td style="text-align: center;padding: 5px;">{{$data['total_qa']}}</td> --}}
-                                        <td style="text-align: center;padding: 5px;">{{ $data['logged_resolv_qa'] }}</td>
-                                        <td style="text-align: center;padding: 5px;">{{ $data['prodcution_qa'] }}</td>
-                                        <td style="text-align: center;padding: 5px;">
-                                            {{ $data['QA'] == 0 ? 'No Activity' : $data['QA'] }}</td>
-                                        {{-- <td style="text-align: left;padding: 5px;">{{ $data['Balance'] }}</td> --}}
+                                        <td>{{ $data['project'] }}</td>
+                                        <td>{{ $data['Chats'] == 0 ? 'No' : 'Yes' }}</td>
+                                        <td>{{ $data['total_ar'] }}</td>
+                                        <td>{{ $data['logged_resolv_ar'] }}</td>
+                                        <td>{{ $data['prodcution_ar'] }}</td>
+                                        <td>{{ $data['Coder'] == 0 ? 'No Activity' : $data['Coder'] }}</td>
+                                        <td>{{ $data['logged_resolv_qa'] }}</td>
+                                        <td>{{ $data['prodcution_qa'] }}</td>
+                                        <td>{{ $data['QA'] == 0 ? 'No Activity' : $data['QA'] }}</td>
                                     </tr>
                                 @endforeach
                             @else
