9ae86176fcdab60caebf56b9ffa0c8e719ddc80e
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2024-11-25 11:01:12 +0530

    hourly detailed mail

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -884,8 +884,10 @@ class ProjectController extends Controller
         try {
             Log::info('Executing Project Hourly Mail logic.');
 
-            $toMailId = ["elanchezhian@annexmed.net", "fabian@annexmed.com", "prabu@annexmed.com","serdeen@annexmed.com","Neel@annexmed.com","Manoj.Achuthan@annexmed.com","radhika@annexmed.com","Gavin@annexmed.com","hemanathan@annexmed.net","vani@annexmed.com","devanathan@annexmed.net"];
-            $ccMailId = ["mgani@caliberfocus.com","margaretmary@annexmed.net","vijayalaxmi@caliberfocus.com"];
+            // $toMailId = ["elanchezhian@annexmed.net", "fabian@annexmed.com", "prabu@annexmed.com","serdeen@annexmed.com","Neel@annexmed.com","Manoj.Achuthan@annexmed.com","radhika@annexmed.com","Gavin@annexmed.com","hemanathan@annexmed.net","vani@annexmed.com","devanathan@annexmed.net"];
+            // $ccMailId = ["mgani@caliberfocus.com","margaretmary@annexmed.net","vijayalaxmi@caliberfocus.com"];
+            $toMailId = ["vijayalaxmi@caliberfocus.com"];
+            $ccMailId = ["vijayalaxmi@caliberfocus.com"];
           
             $mailHeader = "Resolv Project Hourly Report - Trail";
             $projects = collect($this->getProjects());
@@ -1003,14 +1005,15 @@ class ProjectController extends Controller
             $modelClass = "App\\Models\\" . Str::studly($tableName);
             $currentTime = Carbon::now();
             Log::info("Current time: {$currentTime}");
-            if($request->input('requested_date') && $request->input('requested_date') != $currentTime->format('Y-m-d')) {             
+            if($request->input('requested_date') && $request->input('requested_date') == $currentTime->format('Y-m-d')) { 
+                         
                 if ($currentTime->hour < 17) {
                     $startTime = Carbon::yesterday()->setHour(17)->setMinute(0)->setSecond(0);
                         $endTime = Carbon::today()->setHour(5)->setMinute(0)->setSecond(0);
                 } else {
                     $startTime = Carbon::today()->setHour(17)->setMinute(0)->setSecond(0);
                     $endTime = $currentTime;
-                }
+                }  dd( $request->input('requested_date'),  $startTime,$request->input('requested_date')->toda); 
             } else {
                 if ($currentTime->hour < 17) {
                     $startTime = Carbon::yesterday()->setHour(17)->setMinute(0)->setSecond(0);
