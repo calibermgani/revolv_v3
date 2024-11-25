6fba237a92ec713e64f6465c1ea0207e828c0d63
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2024-11-25 15:27:32 +0530

    project hourly web

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -1195,8 +1195,8 @@ class ProjectController extends Controller
     {
         try {
             $projects = collect($this->getProjects());
- dd($request->all());
-            $currentTime = $request['datetime'] != null ? Carbon::createFromFormat('m/d/Y h:i A', $request['datetime']) : Carbon::now();
+
+            $currentTime = $request['datetime'] != null ? Carbon::createFromFormat('Y-m-d', $request['datetime']) : Carbon::now(); dd($request->all(),$currentTime,Carbon::now());
             Log::info("Current time: {$currentTime}");
             if ($currentTime->hour < 17) {
                 if ($currentTime->hour < 5) {
