bb36fdeeaac5884c1f03b64aaa9060bb2e6d3a14
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2024-11-26 10:47:18 +0530

    project hourly detailed eb

--- resources/views/projects/projectHourlyDetailedWeb.blade.php
+++ resources/views/projects/projectHourlyDetailedWeb.blade.php
@@ -1,43 +1,42 @@
 @extends('layouts.app3')
 @section('content')
     <div class="card card-custom custom-card">
-        <p style="margin-top: 5rem;margin-left: 5.5rem;">{{$title}}</p>
-            <div class="card-body py-0 px-7">
-                <div class="table-responsive pb-2">
-                    <table class="table" border="1" style="border-collapse: collapse">
-                        <thead>
-                            <tr>
-                               <th>
-                                    User Name</th>
-                                @foreach ($headers as $header)
-                                    <th
-                                        style="text-align: center;padding: 5px;background-color:#2f75b5;color:#ffffff;font-weight: 100;border-color:black;">
-                                        {{ $header }}</th>
-                                @endforeach
-                               <th>Reached Target</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @if (isset($BodyDetails) && count($BodyDetails) > 0)
-                                @foreach ($BodyDetails as $data)
-                                    <<td>
-                                            {{ $data['user'] != null ? $data['user'] . ' - ' . App\Http\Helper\Admin\Helpers::getUserNameByEmpId($data['user']) : '--' }}
-                                        </td>
-                                        @foreach ($data['hourlyCount'] as $count)
-                                            <td>{{ $count }}</td>
-                                        @endforeach
-                                        <td>
-                                            {{ $data['reachedTarget'] }}</td>
+        <div class="card-body py-0 px-7">
+            <p style="margin-top: 5rem;margin-left: 5.5rem;">{{ $title }}</p>
+            <div class="table-responsive pb-2">
+                <table class="table" border="1" style="border-collapse: collapse">
+                    <thead>
+                        <tr>
+                            <th>User Name</th>
+                            @foreach ($headers as $header)
+                                <th
+                                    style="text-align: center;padding: 5px;background-color:#2f75b5;color:#ffffff;font-weight: 100;border-color:black;">
+                                    {{ $header }}</th>
+                            @endforeach
+                            <th>Reached Target</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @if (isset($BodyDetails) && count($BodyDetails) > 0)
+                            @foreach ($BodyDetails as $data)
+                                <td>
+                                    {{ $data['user'] != null ? $data['user'] . ' - ' . App\Http\Helper\Admin\Helpers::getUserNameByEmpId($data['user']) : '--' }}
+                                    </td>
+                                    @foreach ($data['hourlyCount'] as $count)
+                                        <td>{{ $count }}</td>
+                                    @endforeach
+                                    <td>
+                                        {{ $data['reachedTarget'] }}</td>
                                     </tr>
-                                @endforeach
-                            @else
-                                <tr>
-                                    <td colspan="14" style="text-align: center; padding: 5px;">--No Records--</td>
-                                </tr>
-                            @endif
-                        </tbody>
-                    </table>
-                </div>
+                            @endforeach
+                        @else
+                            <tr>
+                                <td colspan="14" style="text-align: center; padding: 5px;">--No Records--</td>
+                            </tr>
+                        @endif
+                    </tbody>
+                </table>
             </div>
+        </div>
     </div>
 @endsection
