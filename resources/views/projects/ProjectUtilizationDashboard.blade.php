2645f7d856b88144e4c1d3cf0fff706f635e70fa
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2024-11-26 15:31:21 +0530

    utilization dash

--- resources/views/projects/ProjectUtilizationDashboard.blade.php
+++ resources/views/projects/ProjectUtilizationDashboard.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app3')
 @section('content')
 <div class="card card-custom custom-card">
-    <img src="{{ asset('/assets/media/bg/under_construct.png') }}" class="dash_icon">
+    <img src="{{ asset('/assets/media/bg/under_construct.png') }}" style="width: 50%;height: 50%">
 </div>
 @endsection
