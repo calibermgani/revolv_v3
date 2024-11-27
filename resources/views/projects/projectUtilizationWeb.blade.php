a4cf293d2d218eca3c8416bae9c3de6d1f086a93
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2024-11-27 12:25:18 +0530

    hourly export

--- resources/views/projects/projectUtilizationWeb.blade.php
+++ resources/views/projects/projectUtilizationWeb.blade.php
@@ -1,10 +1,24 @@
 @extends('layouts.app3')
 @section('content')
     <div class="card card-custom custom-card" id="project_utilization">
+        <div class="card-body p-0">
+            <div class="card-header border-0 px-4">
+                <div class="row">
+                    <div class="col-md-6">
+                        <span class="project_header" style="margin-left: 4px !important;">Utilization</span>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="row" style="justify-content: flex-end;margin-right:1.4rem">
+                            <div class="outside" href="javascript:void(0);"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         <div class="card card-custom custom-top-border">
             <div class="card-body mr-8 ml-12" id="filter_section">
                 {!! Form::open([
-                    'url' => url('projects/project_work_web') . '?parent=' . request()->parent . '&child=' . request()->child,                   
+                    'url' => url('projects/project_work_web') . '?parent=' . request()->parent . '&child=' . request()->child,
                     'class' => 'form',
                     'id' => 'formSearch',
                     'enctype' => 'multipart/form-data',
@@ -45,7 +59,7 @@
                 <div class="table-responsive pb-2">
 
                     <table class="table table-separate table-head-custom no-footer dtr-column clients_list_filter"
-                         id="project_utilization_table">
+                        id="project_utilization_table">
                         <thead>
                             <tr>
                                 <th>Project</th>
@@ -101,15 +115,29 @@
                 lengthChange: false,
                 searching: true,
                 pageLength: 20,
-            });
+                buttons: [{
+                    "extend": 'excel',
+                    "text": `<span data-dismiss="modal" data-toggle="tooltip" data-placement="left" data-original-title="Export" style="font-size:13px"> <svg xmlns="http://www.w3.org/2000/svg" width="18" height="16" fill="currentColor" class="bi bi-box-arrow-up" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M3.5 6a.5.5 0 0 0-.5.5v8a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5v-8a.5.5 0 0 0-.5-.5h-2a.5.5 0 0 1 0-1h2A1.5 1.5 0 0 1 14 6.5v8a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 14.5v-8A1.5 1.5 0 0 1 3.5 5h2a.5.5 0 0 1 0 1z"/><path fill-rule="evenodd" d="M7.646.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 1.707V10.5a.5.5 0 0 1-1 0V1.707L5.354 3.854a.5.5 0 1 1-.708-.708z"/>
+                             </svg>&nbsp;&nbsp;&nbsp;<span>Export</span></span>`,
+                    "className": 'btn btn-primary-export text-white',
+                    "title": 'Resolv Utilization',
+                    "filename": 'resolv_utilization_report',
+                    "exportOptions": {
+                        "columns": ':not(.notexport)' // Exclude first two columns
+                    }
+                }],
+                dom: "<'row'<'col-md-12'f><'col-md-12't>><'row'<'col-md-5 pt-2'i><'col-md-7 pt-2'p>>"
+            })
+            table.buttons().container()
+                .appendTo('.outside');
         });
         $(document).on('click', '#filter_search', function() {
             KTApp.block('#project_utilization', {
-                    overlayColor: '#000000',
-                    state: 'danger',
-                    opacity: 0.1,
-                    message: 'Fetching...',
-                });
+                overlayColor: '#000000',
+                state: 'danger',
+                opacity: 0.1,
+                message: 'Fetching...',
+            });
         });
     </script>
 @endpush
