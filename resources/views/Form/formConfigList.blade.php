b5ce9c344ff28ed9e3921af86f8a126a7216675b
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2024-11-29 11:49:39 +0530

    form config list design issue

--- resources/views/Form/formConfigList.blade.php
+++ resources/views/Form/formConfigList.blade.php
@@ -62,8 +62,8 @@
                                         data-href="{{ route('formEdit', ['parent' => request()->parent, 'child' => request()->child, 'project_id' => $project_id_encode, 'sub_project_id' => $sub_project_id_encode]) }}" style="cursor:pointer !important">
                                         <td style="width: 13%"><input type="hidden" value="{{$data->project_id}}">{{ $projectName->aims_project_name }}</td>
                                         <td style="width: 10%"><input type="hidden" value="{{$data->sub_project_id}}">{{ $subProjectName == '--' ? '--' : $subProjectName->sub_project_name }}</td>
-                                        <td style="width: 10%">{{$data->label_name}}</td>
-                                        <td class="project_delete" data-value="{{$loop->iteration}}"><i
+                                        <td style="width: 10%">{{$data->label_names}}</td>
+                                        <td class="project_delete" data-value="{{$loop->iteration}}" style="width: 3%"><i
                                             class="fa fas fa-trash text-danger icon-circle2 ml-1 mt-0 record_delete"></i></a></td>
                                     </tr>
                                     @endif
