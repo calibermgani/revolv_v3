24dcaea8f9452857293513880247f8344e0d9203
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2024-11-29 11:19:18 +0530

    form config list design issue

--- resources/views/Form/formConfigList.blade.php
+++ resources/views/Form/formConfigList.blade.php
@@ -15,7 +15,7 @@
                         </span> --}}
                         <span class="project_header">Project Creation List</span>
                     </div>
-                    <div style="margin-bottom:-2rem"
+                    <div
                         class="d-flex flex-row justify-content-between align-items-center float-right ml-2">
 
                         <a class="btn btn-white-black font-weight-bolder btn-sm mr-1"
@@ -63,7 +63,7 @@
                                             data-href="{{ route('formEdit', ['parent' => request()->parent, 'child' => request()->child, 'project_id' => $project_id_encode, 'sub_project_id' => $sub_project_id_encode]) }}" style="cursor:pointer !important">
                                             <td><input type="hidden" value="{{$data->project_id}}">{{ $projectName->aims_project_name }}</td>
                                             <td><input type="hidden" value="{{$data->sub_project_id}}">{{ $subProjectName == '--' ? '--' : $subProjectName->sub_project_name }}</td>
-                                            <td>--</td>
+                                            <td>{{ wordwrap($data->label_names, 120, '<br>') }}</td>
                                             <td class="project_delete" data-value="{{$loop->iteration}}"><i
                                                 class="fa fas fa-trash text-danger icon-circle2 ml-1 mt-0 record_delete"></i></a></td>
                                         </tr>
