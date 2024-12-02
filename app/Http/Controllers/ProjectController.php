b4a14a9d4b01f41196bb70a72dcec7aed45c0e56
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2024-12-02 13:44:57 +0530

    utilization werv

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -1487,11 +1487,11 @@ class ProjectController extends Controller
     }
 }
 public function getProjectCounts($projectId,$yesterDayStartDate,$yesterDayEndDate)
-{
+{dd(explode(',', $projectId));
     try {
         // Retrieve AR and QA counts from Cache
         // $totalAR = Cache::get("project_{$projectId}_ar_count", 0); // Default to 0 if not found
-        $totalAR = Cache::get( 'project_' . $projectId . '_ar_count', 0); 
+        $totalAR = Cache::get( 'project_' . explode(',', $projectId) . '_ar_count', 0); 
         // $totalQA = Cache::get("project_{$projectId}_qa_count", 0); // Default to 0 if not found
      dd('totalAR',$totalAR);
         $loggedResolvAR = 0;
