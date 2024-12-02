4866dfad6038d5811915a079541ac6ee623f1a45
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2024-12-02 12:42:52 +0530

    utilization werv

--- app/Jobs/GetTotalARCountJob.php
+++ app/Jobs/GetTotalARCountJob.php
@@ -34,7 +34,7 @@ class GetTotalARCountJob implements ShouldQueue
 Log::info("Processed Project IDs", ['projectIds' => $this->projectIds]);
 
 // Create a unique cache key from project IDs
-$cacheKey = 'project_' . implode('_', $this->projectIds) . '_ar_count';
+$cacheKey = 'project_' . $this->projectIds . '_ar_count';
 
 // Store the data in the cache
 Cache::put($cacheKey, $data, now()->addMinutes(30));
