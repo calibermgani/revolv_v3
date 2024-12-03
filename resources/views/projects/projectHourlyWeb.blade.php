1f93e7c680f96894fa07e4b1e90ab396a2cebd7e
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2024-12-03 10:12:22 +0530

    utilization werv

--- resources/views/projects/projectHourlyWeb.blade.php
+++ resources/views/projects/projectHourlyWeb.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app3')
 @section('content')
-    <div class="card card-custom custom-card">
+    <div class="card card-custom custom-card" id="hourly_card">
         <div class="card-body p-0">
             <div class="card-header border-0 px-4">
                 <div class="row">
@@ -118,7 +118,7 @@
                     opacity: 0.1,
                     message: 'Fetching...',
                 });
-            var table = $("#project_hourly_table").DataTable({
+            var table = $("#hourly_card").DataTable({
                 processing: true,
                 ordering: true,
                 clientSide: true,
@@ -145,7 +145,7 @@
                 location.reload();
             })
             $(document).on('click', '#filter_search', function() {
-                KTApp.block('#project_hourly_table', {
+                KTApp.block('#hourly_card', {
                     overlayColor: '#000000',
                     state: 'danger',
                     opacity: 0.1,
