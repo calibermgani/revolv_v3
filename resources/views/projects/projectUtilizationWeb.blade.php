26431879d36c8ec293eceb5c400287ab73bc35c5
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2024-12-03 10:09:19 +0530

    utilization werv

--- resources/views/projects/projectUtilizationWeb.blade.php
+++ resources/views/projects/projectUtilizationWeb.blade.php
@@ -110,6 +110,12 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
     <script>
         $(document).ready(function() {
+            KTApp.block('#project_utilization', {
+                overlayColor: '#000000',
+                state: 'danger',
+                opacity: 0.1,
+                message: 'Fetching...',
+            });
             var table = $("#project_utilization_table").DataTable({
                 processing: true,
                 ordering: true,
@@ -152,6 +158,7 @@
                             $(this).find(".logged_resolv_ar").text(data.logged_resolv_ar);
                             $(this).find(".logged_resolv_qa").text(data.logged_resolv_qa);
                             // $(this).find(".total-qa").text(data.total_qa);
+                            KTApp.unblock('#project_utilization');
                         }
                     })
                     .catch(error => console.error("Error fetching AR/QA counts:", error));
