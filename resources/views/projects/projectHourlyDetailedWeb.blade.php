77e21202fbb3f56cc800595d3ee670ec81e59dda
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2024-12-05 12:40:10 +0530

    actuval targetm in detailed info web

--- resources/views/projects/projectHourlyDetailedWeb.blade.php
+++ resources/views/projects/projectHourlyDetailedWeb.blade.php
@@ -54,6 +54,7 @@
                 lengthChange: false,
                 searching: true,
                 pageLength: 20,
+                "order": [[15, "asc"]]
             });
         });
     </script>
