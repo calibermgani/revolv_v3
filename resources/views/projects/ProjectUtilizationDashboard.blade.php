57848459c8193d363075c63a9ab64b6361817719
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2024-12-06 17:14:20 +0530

    utilization dash

--- resources/views/projects/ProjectUtilizationDashboard.blade.php
+++ resources/views/projects/ProjectUtilizationDashboard.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app3')
 @section('content')
 <div class="card card-custom custom-card">
-    <img src="{{ asset('/assets/media/bg/dash_imh.jpg') }}" style="width: 50%;height: 50%">
+    {{-- <img src="{{ asset('/assets/media/bg/dash_imh.jpg') }}" style="width: 50%;height: 50%"> --}}
+    <h4>Coming Soon...........</h4>
 </div>
 @endsection
