c28872f97fcd8bb9bc44630d5586f17a2fc286b3
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2024-12-06 17:18:26 +0530

    utilization dash

--- resources/views/projects/ProjectUtilizationDashboard.blade.php
+++ resources/views/projects/ProjectUtilizationDashboard.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app3')
 @section('content')
 <div class="card card-custom custom-card d-flex justify-content-center align-items-center" style="height: 100%;">
-    <h4>Coming Soon...........</h4>
+    <h4>COMING SOON.......</h4>
 </div>
 @endsection
