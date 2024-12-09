692c3361350cb85a04261d3d19906194d6418041
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2024-12-09 13:26:30 +0530

    utilization mail

--- resources/views/emails/projectWorkMail.blade.php
+++ resources/views/emails/projectWorkMail.blade.php
@@ -66,11 +66,12 @@
                             <td style="text-align: center;padding: 5px;">{{ $data['Chats'] == 0 ? 'No' : 'Yes' }}</td>
                             {{-- <td style="text-align: center;padding: 5px;">{{ $data['total_ar']}}</td> --}}
                             <td class="total-ar"  style="text-align: center;padding: 5px;"></td>
-                            <td style="text-align: center;padding: 5px;">{{ $data['logged_resolv_ar']}}</td>
+                            {{-- <td style="text-align: center;padding: 5px;">{{ $data['logged_resolv_ar']}}</td> --}}
+                            <td class="logged_resolv_ar" style="text-align: center;padding: 5px;"></td>
                             <td style="text-align: center;padding: 5px;">{{$data['prodcution_ar']}}</td>
                             <td style="text-align: center;padding: 5px;">{{ $data['Coder'] == 0 ? 'No Activity' : $data['Coder']}}</td>
                              {{-- <td style="text-align: center;padding: 5px;">{{ $data['logged_resolv_qa']}}</td> --}}
-                             <td class="logged_resolv_qa"></td>
+                             <td class="logged_resolv_qa" style="text-align: center;padding: 5px;"></td>
                             <td style="text-align: center;padding: 5px;">{{$data['prodcution_qa']}}</td>
                             <td style="text-align: center;padding: 5px;">{{ $data['QA'] == 0 ? 'No Activity' : $data['QA']}}</td>
                             
