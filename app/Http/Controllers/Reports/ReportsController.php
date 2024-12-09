c1983adfcf4e9fe3b36b84a6c4198e36a15783b8
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2024-12-09 17:36:29 +0530

    production report

--- app/Http/Controllers/Reports/ReportsController.php
+++ app/Http/Controllers/Reports/ReportsController.php
@@ -553,7 +553,7 @@ class ReportsController extends Controller
                         $productionReportList = $modelClass::select('CE_emp_id')->where('chart_status','CE_Completed')->groupBy('CE_emp_id')->get();
                     }
                     foreach ($productionReportList as $data) {   
-                        $productionReporArray['arName'][] = $data['CE_emp_id'] != null
+                        $productionReporArray['arName']= $data['CE_emp_id'] != null
                         ? Helpers::getUserNameByEmpId($data['CE_emp_id'])
                         : '--';
                     }
