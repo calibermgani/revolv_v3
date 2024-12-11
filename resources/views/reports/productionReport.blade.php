dfe62b0fe3b47efa0f4a30c73dba0fc529e9ac2e
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2024-12-11 14:07:30 +0530

    production report

--- resources/views/reports/productionReport.blade.php
+++ resources/views/reports/productionReport.blade.php
@@ -169,10 +169,8 @@
                                     );
                                     if($target !== '--' && $target !== null) {
                                         $target_per_hour = (int) $target->target_per_day / 8;
-                                        $totalWorkTime = $totalWorkTime ?: '00:00:01'; 
-                                        list($hours, $minutes, $seconds) = explode(':', $totalWorkTime);
-                                        // $totalWorkTimeInHours = $hours + ($minutes / 60) + ($seconds / 3600);
-                                         $totalSeconds = ($hours * 3600) ;
+                                  
+                                         $totalSeconds = (8 * 3600) ;
                                 
                                         // $target_per_hour = (int) $target->target_per_day / 8;
                                         $target_per_minute = (int) $target->target_per_hour / 60;
