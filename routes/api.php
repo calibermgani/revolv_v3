31b3840a7811305225fc7143fcd6af5b7ca056d7
calibermgani <mgani@medcubics.com>
2024-12-27 15:20:41 +0530

    The Queens Health System

--- routes/api.php
+++ routes/api.php
@@ -130,6 +130,9 @@ Route::group(['prefix' => 'projects'], function() {
     Route::any('precision_billing_and_consulting_services_ar_duplicate', 'App\Http\Controllers\ProjectAutomationController@precisionBillingAndConsultingServicesArDuplicates');
     Route::any('siouxland_mental_health_center_ar', 'App\Http\Controllers\ProjectAutomationController@SiouxlandMentalHealthCenterAR');
     Route::any('siouxland_mental_health_center_ar_duplicate', 'App\Http\Controllers\ProjectAutomationController@SiouxlandMentalHealthCenterARArDuplicates');
+    Route::any('the_queens_health_system_ar', 'App\Http\Controllers\ProjectAutomationController@TheQueensHealthSystemAR');
+    Route::any('the_queens_health_system_ar_duplicate', 'App\Http\Controllers\ProjectAutomationController@TheQueensHealthSystemArDuplicates');
+    
 
 
 });
