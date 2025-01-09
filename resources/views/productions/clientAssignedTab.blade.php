57a717c1152f0d0399ea64113e542522b0ec1b41
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2025-01-09 15:11:17 +0530

    manual btn issue

--- resources/views/productions/clientAssignedTab.blade.php
+++ resources/views/productions/clientAssignedTab.blade.php
@@ -1643,10 +1643,11 @@ nav{
                                 inclass.each(function(element) {
 
                                     var label_id = $(this).attr('id');
-                                    if ($(this).val() == '') {
-                                        if ($(this).val() == '') {
+                                    if ($(this).val() === '') {console.log($(this).val(),'val');
+                                    
+                                        if ($(this).val() === '') {
                                             e.preventDefault();
-                                            $(this).css('border-color', 'red', 'important');
+                                            $(this).css('border-color', 'red', '!important');
                                             inputTypeValue =1;console.log(inputTypeValue,'class Value',classValue,$(this).val());
                                                 
                                         } else {
