eecc4c20013dc1c355799eef5ec3f84f435abe94
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2025-01-09 15:59:50 +0530

    manual btn issue

--- resources/views/productions/clientAssignedTab.blade.php
+++ resources/views/productions/clientAssignedTab.blade.php
@@ -1638,42 +1638,24 @@ nav{
                                     'class');
                             if (classValue !== undefined) {
                                 var classes = classValue.split(' ');
-                                var inclass = $(`${fieldType}[name="${fieldNameVal}"]`);
-                                    console.log(classes[1], 'inclass', inclass);
+                                inputclass.push($('.' + classes[1]));
+                                inclass = $('.' + classes[1]);console.log(inputTypeValue,'inclass',inclass);
+                                inclass.each(function(element) {
 
-                                    inclass.each(function () {
-                                        var $this = $(this);
-                                        var label_id = $this.attr('id');
-                                        var value = $this.val();
-
-                                        if (value === '') {
-                                            e.preventDefault();
-                                            $this.css('border-color', 'red');
-                                            inputTypeValue = 1;
-                                            console.log(inputTypeValue, 'class Value', value);
-                                        } else {
-                                            $this.css('border-color', '');
-                                            inputTypeValue = 0;
-                                        }
-                                    });
-                                // inputclass.push($('.' + classes[1]));
-                                // inclass = $('.' + classes[1]);console.log(inputTypeValue,'inclass',inclass);
-                                // inclass.each(function(element) {
-
-                                //     var label_id = $(this).attr('id');
-                                    // if ($(this).val() === '') {console.log($(this).val(),'val');
+                                    var label_id = $(this).attr('id');
+                                    if ($(this).val() === '') {console.log(element,'val');
                                     
-                                    //     if ($(this).val() === '') {
-                                    //         e.preventDefault();
-                                    //         $(this).css('border-color', 'red', '!important');
-                                    //         inputTypeValue =1;console.log(inputTypeValue,'class Value',classValue,$(this).val());
+                                        if ($(this).val() === '') {
+                                            e.preventDefault();
+                                            $(this).css('border-color', 'red', '!important');
+                                            inputTypeValue =1;console.log(inputTypeValue,'class Value',classValue,$(this).val());
                                                 
-                                    //     } else {
-                                    //         $(this).css('border-color', '');
-                                    //         inputTypeValue = 0;console.log(inputTypeValue,'class Value 0',classValue);
-                                    //     }
-                                    //     return false;
-                                    // }
+                                        } else {
+                                            $(this).css('border-color', '');
+                                            inputTypeValue = 0;console.log(inputTypeValue,'class Value 0',classValue);
+                                        }
+                                        return false;
+                                    }
                                     // if(label_id.substring(0, 3) == "cpt") {
                                     //     var textValue = $(this).val();
                                     //     if(textValue.length < 4) {
@@ -1686,7 +1668,7 @@ nav{
                                     //         js_notification('error', "The ICD value must be between 3 and 7 characters long" );
                                     //     }
                                     // }
-                                // });
+                                });
                             }
                         });
 
