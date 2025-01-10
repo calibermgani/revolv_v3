b801f95abb04d06e21dd097beceef212080e37c4
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2025-01-10 16:05:40 +0530

    manual add btn issue

--- resources/views/productions/clientAssignedTab.blade.php
+++ resources/views/productions/clientAssignedTab.blade.php
@@ -1508,7 +1508,6 @@ nav{
             });
             var manualProjectDuplicateColumns = @json($attributes);var duplicateColumnData = []; 
             $(document).on('click', '#project_assign_save', function(e) {
-                 console.log(inputTypeValue + inputTypeRadioValue + duplicateValue );
                 e.preventDefault();
                 $('#formConfiguration').serializeArray().map(function(input) {
                     labelName = input.name.replace('[]', '');
