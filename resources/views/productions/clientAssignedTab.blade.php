0e2fe41ee2b1f207ea794ff6c8a749c1bd4e62bc
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2025-01-13 12:12:01 +0530

    manual btn

--- resources/views/productions/clientAssignedTab.blade.php
+++ resources/views/productions/clientAssignedTab.blade.php
@@ -250,13 +250,13 @@ use Carbon\Carbon;
                                                         <div class="col-md-10">
                                                             @if ($options == null)
                                                                 @if ($inputType != 'date_range')
-                                                                    {!! Form::$inputType($columnName,isset($searchData) && !empty($searchData) && isset($searchData[$columnName]) && $searchData[$columnName] ? $searchData[$columnName] : null, [
+                                                                    {{-- {!! Form::$inputType($columnName,isset($searchData) && !empty($searchData) && isset($searchData[$columnName]) && $searchData[$columnName] ? $searchData[$columnName] : null, [
                                                                         'class' => 'form-control ' . $columnName . ' white-smoke pop-non-edt-val',
                                                                         'autocomplete' => 'none',
                                                                         'style' => 'cursor:pointer',
                                                                         'rows' => 3,
                                                                         'id' => $columnName,
-                                                                    ]) !!}
+                                                                    ]) !!} --}}
                                                                 @else
                                                                     {!! Form::text($columnName, null, [
                                                                         'class' => 'form-control date_range daterange_' . $columnName . ' white-smoke pop-non-edt-val',
