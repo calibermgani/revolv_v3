60e5c5adcaa4815f4950e46062d75c07ff754fee
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2025-01-13 09:48:07 +0530

    manual btn

--- resources/views/productions/clientAssignedTab.blade.php
+++ resources/views/productions/clientAssignedTab.blade.php
@@ -1653,10 +1653,11 @@ nav{
                             if (classValue !== undefined) {
                                 var classes = classValue.split(' ');
                                 inputclass.push($('.' + classes[1]));
+                                inclass = $('.' + classes[1]);
                                 inclass = inclass.filter(function(index, element) {
                                     return inclass.toArray().findIndex((el) => el.isEqualNode(element)) === index;
                                 });console.log(inclass,'inclass');
-                                // inclass = $('.' + classes[1]);
+                                
                                 
                                 inclass.each(function(element) {
 
