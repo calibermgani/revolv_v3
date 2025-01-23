063edcc00086935163bd085c3c1d8ff2e4fa20a0
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2025-01-23 13:36:30 +0530

    hourly

--- resources/views/projects/projectHourlyWeb.blade.php
+++ resources/views/projects/projectHourlyWeb.blade.php
@@ -133,7 +133,7 @@
                                             </a>
                                         </td>
                                         @foreach ($data['hourlyCount'] as $count)
-                                            <td  style={{$data['achievedPercentage'] < (int)$prjBillableFTE* $prjSLATarget/8 ? "color:red" : "color:black"}}>{{ $count }}</td>
+                                            <td  style={{$count < (int)$prjBillableFTE* $prjSLATarget/8 ? "color:red" : "color:black"}}>{{ $count }}</td>
                                         @endforeach
                                         <td>{{$prjMgrName}}</td>
                                         <td>{{$prjBillableFTE}}</td>
