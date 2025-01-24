34adb3414b13bc709a640dfef9d11b88a945b34a
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2025-01-24 13:24:39 +0530

    sage wood

--- resources/views/projects/projectHourlyWeb.blade.php
+++ resources/views/projects/projectHourlyWeb.blade.php
@@ -148,7 +148,7 @@
                                             {{ $count }}
                                         </td>
                                         @else
-                                        <td style="color:red !important">{{ $count }}</td>
+                                        <td style="color:red !important">--</td>
                                         @endif
                                         @endforeach
                                         
