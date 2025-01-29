8614cef6a85ea09367342e6ef70827c692acad19
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2025-01-29 15:55:57 +0530

    play btn disable refresh

--- resources/views/productions/clientAssignedTab.blade.php
+++ resources/views/productions/clientAssignedTab.blade.php
@@ -18,7 +18,7 @@ use Carbon\Carbon;
                                 <div class="col-md-6">
                                     <div class="row" style="justify-content: flex-end;margin-right:1.4rem">
                                         @if ($loginEmpId  == "Admin" || strpos($empDesignation, 'Manager') !== false || strpos($empDesignation, 'VP') !== false || strpos($empDesignation, 'Leader') !== false || strpos($empDesignation, 'Team Lead') !== false || strpos($empDesignation, 'CEO') !== false || strpos($empDesignation, 'Vice') !== false)
-                                            <div class="mb-lg-0 mb-6" id="play_btn_reset"  title="Claims Refresh">
+                                            <div class="mb-lg-0 mb-6" id="play_btn_reset"  title="Claims Refresh" style="display:none">
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-arrow-clockwise mt-2" viewBox="0 0 16 16" style="cursor: pointer;">
                                                 <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2z"/>
                                                 <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466"/>
