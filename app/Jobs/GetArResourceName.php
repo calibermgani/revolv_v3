c455ad9be13e2cb426d789e81382a473487d0619
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2025-01-30 12:48:45 +0530

    ply btn reset in client assigned tab

---
+++ app/Jobs/GetArResourceName.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Cache;
+
+class GetArResourceName implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public string $projectId;
+    public function __construct(string $projectId)
+    {
+        $this->projectId = $projectId;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $data = app()->call('App\Http\Helper\Admin\Helpers@getArResourceName', [
+            'project_id' => $this->projectId
+        ]);      
+        $cacheKey = 'project_'.$this->projectId.'arResourceName' ;
+        Cache::put($cacheKey, $data, now()->addMinutes(30));
+  
+    }
+}
