80f5e6fd20607b1cbfca98a98b01e0bef7bff838
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2025-02-20 14:36:39 +0530

    Missoula Bone

--- app/Http/Controllers/ProjectAutomationController.php
+++ app/Http/Controllers/ProjectAutomationController.php
@@ -3912,7 +3912,7 @@ public function MissoulaBoneANDJointSurgeryCenterLLCModmedAR(Request $request)
                'patient_name' => isset($request->patient_name) && $request->patient_name != "NULL" ? $request->patient_name : NULL,                      
                'primary_provider' => isset($request->primary_provider) && $request->primary_provider != "NULL" ? $request->primary_provider : NULL,  
                'total_ar_aging' => isset($request->total_ar_aging) && $request->total_ar_aging != "NULL" ? $request->total_ar_aging : NULL,  
-               'invoke_date' => carbon::now()->format('Y-m-d')           
+              // 'invoke_date' => carbon::now()->format('Y-m-d')           
            ];
            
 
@@ -3947,13 +3947,30 @@ public function MissoulaBoneANDJointSurgeryCenterLLCModmedAR(Request $request)
                     'patient_dob' => isset($request->patient_dob) && $request->patient_dob != "NULL" ? $request->patient_dob : NULL,
                     'payer_policy_number' => isset($request->payer_policy_number) && $request->payer_policy_number != "NULL" ? $request->payer_policy_number : NULL,
                     'total_ar_aging' => isset($request->total_ar_aging) && $request->total_ar_aging != "NULL" ? $request->total_ar_aging : NULL,               
-                    'invoke_date' => date('Y-m-d'),
                     'CE_emp_id' => isset($request->CE_emp_id) && $request->CE_emp_id != '-' && $request->CE_emp_id != "NULL" ? $request->CE_emp_id : NULL,
                     'QA_emp_id' => isset($request->QA_emp_id) && $request->QA_emp_id != '-' && $request->QA_emp_id != "NULL" ? $request->QA_emp_id : NULL,
                     'updated_at'=> carbon::now()->format('Y-m-d H:i:s')
                 ]);
+                return response()->json(['message' => 'Existing Record Updated Successfully']);
+            } else {
+                  MbjsclMbjModmed::insert([
+                    'dos' => isset($request->dos) && $request->dos != "NULL" ? $request->dos : NULL,  
+                    'primary_provider' => isset($request->primary_provider) && $request->primary_provider != "NULL" ? $request->primary_provider : NULL, 
+                    'primary_provider_npi' => isset($request->primary_provider_npi) && $request->primary_provider_npi != "NULL" ? $request->primary_provider_npi : NULL,                      
+                    'responsible_party' => isset($request->responsible_party) && $request->responsible_party != "NULL" ? $request->responsible_party : NULL,  
+                    'bill_id' => isset($request->bill_id) && $request->bill_id != "NULL" ? $request->bill_id : NULL,  
+                    'patient_name' => isset($request->patient_name) && $request->patient_name != "NULL" ? $request->patient_name : NULL,  
+                    'patient_dob' => isset($request->patient_dob) && $request->patient_dob != "NULL" ? $request->patient_dob : NULL,
+                    'payer_policy_number' => isset($request->payer_policy_number) && $request->payer_policy_number != "NULL" ? $request->payer_policy_number : NULL,
+                    'total_ar_aging' => isset($request->total_ar_aging) && $request->total_ar_aging != "NULL" ? $request->total_ar_aging : NULL,               
+                    'invoke_date' => date('Y-m-d'),
+                    'CE_emp_id' => isset($request->CE_emp_id) && $request->CE_emp_id != '-' && $request->CE_emp_id != "NULL" ? $request->CE_emp_id : NULL,
+                    'QA_emp_id' => isset($request->QA_emp_id) && $request->QA_emp_id != '-' && $request->QA_emp_id != "NULL" ? $request->QA_emp_id : NULL,
+                    'chart_status' => "CE_Assigned",
+                     ]);
+                     return response()->json(['message' => 'Existing Record reinserted Successfully']);
             }
-            return response()->json(['message' => 'Existing Record Updated Successfully']);
+           
         }
     } catch (\Exception $e) {
         $e->getMessage();
