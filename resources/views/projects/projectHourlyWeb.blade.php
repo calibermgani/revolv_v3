f6237ba7ff041552994879827d5de85eed91bbb8
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2025-03-19 10:22:11 +0530

    hourly web code projects from resolv

--- resources/views/projects/projectHourlyWeb.blade.php
+++ resources/views/projects/projectHourlyWeb.blade.php
@@ -89,7 +89,7 @@
 
                             @if (isset($mailBody) && count($mailBody) > 0)
                             @php
-                               $prjDetailsList = App\Http\Helper\Admin\Helpers::getProjectInformationForHourlyWeb($projectIds);   dd($prjDetailsList,$mailBody);                        
+                               $prjDetailsList = App\Http\Helper\Admin\Helpers::getProjectInformationForHourlyWeb($projectIds);   dd($projectIds,$prjDetailsList,$mailBody);                        
                             @endphp
                                 @foreach ($mailBody as $dKey => $data)
                                 @php                              
