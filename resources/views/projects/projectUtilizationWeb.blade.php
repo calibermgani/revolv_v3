d03114663537efd058dda9b4fcd1a53d4cfb4c8d
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2025-03-21 15:50:40 +0530

    Detailed Information web

--- resources/views/projects/projectUtilizationWeb.blade.php
+++ resources/views/projects/projectUtilizationWeb.blade.php
@@ -93,7 +93,7 @@
                                         <td class="logged_resolv_ar"></td>
                                         <td>{{ $data['prodcution_ar'] }}</td>
                                         <td>{{ $data['Coder'] == 0 ? 'No Activity' : $data['Coder'] }}</td>
-                                        <td>{{  $prjDetailsList != '--' &&  $prjDetailsList != null ? $prjDetailsList[$key]['aims_count'] :  $prjDetailsList}}</td>
+                                        <td style={{$prjDetailsList != '--' &&  $prjDetailsList != null ? ($data['Coder'] !=0 && $data['Coder'] == $prjDetailsList[$key]['aims_count'] ? "color:green" : "color:red") : "color:red"}}>{{  $prjDetailsList != '--' &&  $prjDetailsList != null ? $prjDetailsList[$key]['aims_count'] :  $prjDetailsList}}</td>
                                         {{-- <td>{{ $data['logged_resolv_qa'] }}</td> --}}
                                         <td class="logged_resolv_qa"></td>
                                         <td>{{ $data['prodcution_qa'] }}</td>
