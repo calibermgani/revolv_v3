26322b2e3c3f3f14bbd9b6ffa04a84ef891b5aad
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2025-04-07 09:52:59 +0530

    utilization Push

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -1734,11 +1734,11 @@ class ProjectController extends Controller
                $clientDetails = array();
                foreach ($clientList as $clientData) {
                 $clientName['id'] = $clientData->project_id;
-                $clientName['client_name'] = Helpers::projectName($clientData->project_id)->aims_project_name;
+                $clientName['client_name'] = $clientData->project_id != null ? Helpers::projectName($clientData->project_id)->aims_project_name : null;
                 $clientName['subprject_name'] =  $clientData->project_id != null ? subproject::where('project_id', $clientData->project_id)->where('sub_project_id', $clientData->sub_project_id)->pluck('sub_project_name', 'sub_project_id')->toArray(): [];
-                $clientDetails[] = $clientName;    dd($clientDetails);
+                $clientDetails[] = $clientName;    
             }
-        
+            dd($clientDetails);
            return $clientDetails;
             } catch (\Exception $e) {
                 Log::debug($e->getMessage());
