72f1face4c6795565ddf2af6a6cf749166f85140
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2025-04-07 09:36:40 +0530

    utilization Push

--- resources/views/projects/projectUtilizationWeb.blade.php
+++ resources/views/projects/projectUtilizationWeb.blade.php
@@ -62,13 +62,13 @@
                         id="project_utilization_table">
                         <thead>
                             <tr>
-                                <th>Projectt</th>
+                                <th>Project</th>
                                 <th>Inventory Uploaded</th>
                                 <th>Total Users - AR</th>
                                 <th>Logged Resolv - AR</th>
                                 <th>Production Users - AR</th>
                                 <th>AR</th>
-                                <th>AIMS Production</th>
+                                {{-- <th>AIMS Production</th> --}}
                                 <th>Logged Resolv - QA</th>
                                 <th>Production - QA</th>
                                 <th>QA</th>
@@ -93,7 +93,7 @@
                                         <td class="logged_resolv_ar"></td>
                                         <td>{{ $data['prodcution_ar'] }}</td>
                                         <td>{{ $data['Coder'] == 0 ? 'No Activity' : $data['Coder'] }}</td>
-                                        <td style={{$prjDetailsList != '--' &&  $prjDetailsList != null ? ($data['Coder'] !=0 && $data['Coder'] == $prjDetailsList[$key]['aims_count'] ? "color:green" : "color:red") : "color:red"}}>{{  $prjDetailsList != '--' &&  $prjDetailsList != null ? $prjDetailsList[$key]['aims_count'] :  $prjDetailsList}}</td>
+                                        {{-- <td style={{$prjDetailsList != '--' &&  $prjDetailsList != null ? ($data['Coder'] !=0 && $data['Coder'] == $prjDetailsList[$key]['aims_count'] ? "color:green" : "color:red") : "color:red"}}>{{  $prjDetailsList != '--' &&  $prjDetailsList != null ? $prjDetailsList[$key]['aims_count'] :  $prjDetailsList}}</td> --}}
                                         {{-- <td>{{ $data['logged_resolv_qa'] }}</td> --}}
                                         <td class="logged_resolv_qa"></td>
                                         <td>{{ $data['prodcution_qa'] }}</td>
@@ -102,7 +102,7 @@
                                 @endforeach
                             @else
                                 <tr>
-                                    <td colspan="10" style="text-align: center; padding: 5px;">--No Records--</td>
+                                    <td colspan="9" style="text-align: center; padding: 5px;">--No Records--</td>
                                 </tr>
                             @endif
                         </tbody>
