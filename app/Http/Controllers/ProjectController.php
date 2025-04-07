4f9e108b2582fe2eaf6069f5cf65b5ae3fc70b97
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2025-04-07 12:22:23 +0530

    project Name fix

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -1571,9 +1571,9 @@ class ProjectController extends Controller
                 $mailBody = $projectIds = $subProjectIds = [];
 
                 // Process each project
-                foreach ($projects as $project) {dd($project);
+                foreach ($projects as $project) {
                     //$prjName = Helpers::projectName($project['id'])->project_name ?? null;
-                    $prjDetails = $request->project_id != null ? Helpers::projectName($project['id']) : null;dd($prjDetails);
+                    $prjDetails = $project['id'] != null ? Helpers::projectName($project['id']) : null;
                     $prjName = $prjDetails && $prjDetails != null ? $prjDetails->project_name : null;
                     if ($prjName === null) {
                         Log::warning("Project name is null for project ID {$project['id']}");
