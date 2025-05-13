3dd5f66c06195e6dc393c48e05dd5a1e1cfe23c1
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2025-05-13 15:58:39 +0530

    qa debug

--- app/Http/Controllers/QA/QAProductionController.php
+++ app/Http/Controllers/QA/QAProductionController.php
@@ -263,12 +263,12 @@ class QAProductionController extends Controller
                         $existingCallerChartsWorkLogsInprocess = CallerChartsWorkLogs::where('project_id',$decodedProjectName)->where('sub_project_id',$subProjectId)->where('emp_id',$loginEmpId)->where('record_status','QA_Inprocess')->orderBy('id','desc')->pluck('record_id')->toArray();
                         $existingCallerChartsWorkLogs = CallerChartsWorkLogs::where('project_id', $decodedProjectName)->where('sub_project_id', $subProjectId)->where('emp_id', $loginEmpId)->where('end_time', null)->whereIn('record_status', ['QA_Assigned','QA_Inprocess'])->orderBy('id', 'desc')->pluck('record_id')->toArray();
                         $assignedProjectDetails = $query->whereIn('chart_status',['CE_Completed','QA_Inprocess','Auto_Close'])->where('qa_work_status','Sampling')->where('QA_emp_id',$loginEmpId);
+                        if (!empty($existingCallerChartsWorkLogs)) {
                             $assignedProjectDetails = $assignedProjectDetails->orderByRaw('FIELD(id, ' . implode(',', $existingCallerChartsWorkLogs) . ') DESC'); 
                         }
                         if (!empty($existingCallerChartsWorkLogsInprocess)) {
                             $assignedProjectDetails = $assignedProjectDetails->orderByRaw('FIELD(id, ' . implode(',', $existingCallerChartsWorkLogsInprocess) . ') DESC');
                         }
-                        if (!empty($existingCallerChartsWorkLogs)) {
                         $assignedProjectDetails = $assignedProjectDetails->orderBy('id', 'ASC')->paginate(50);
                         $assignedCount = $modelClass::whereIn('chart_status',['CE_Completed','QA_Inprocess','Auto_Close'])->where('qa_work_status','Sampling')->where('QA_emp_id', $loginEmpId)->count();
                         $completedCount = $modelClass::where('chart_status', 'QA_Completed')->where('QA_emp_id', $loginEmpId)->whereBetween('updated_at',[$startDate,$endDate])->count();
