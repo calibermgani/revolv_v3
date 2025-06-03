4a13ac02942fab38bad7909f46989b809c2535d1
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2025-06-03 14:19:02 +0530

    user wise project report

--- resources/views/reports/userProjectReport.blade.php
+++ resources/views/reports/userProjectReport.blade.php
@@ -103,6 +103,7 @@
                            @php
                           
                               $subProjectName = $project['prj_id'] != null && $project['sub_prj_id'] != null ? App\Http\Helper\Admin\Helpers::subProjectName($project['prj_id'], $project['sub_prj_id'])['sub_project_name'] : '--'; 
+                              dd('clientIds:',$clientIds,'subPrjIds:',$subPrjIds,'formProjectIds:',$formProjectIds,$formConfigurationDetails);
                              dd($subProjectName !== '--' ,  isset($formConfigurationDetails[$project['sub_prj_id']]),$formConfigurationDetails[$project['sub_prj_id']] == $project['prj_id'],$formConfigurationDetails[$project['sub_prj_id']] , $project['prj_id'],$formConfigurationDetails);
                            @endphp
                            @if($subProjectName !== '--' &&  isset($formConfigurationDetails[$project['sub_prj_id']]) &&  $formConfigurationDetails[$project['sub_prj_id']] == $project['prj_id'])        
