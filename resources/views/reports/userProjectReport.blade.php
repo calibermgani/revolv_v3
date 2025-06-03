4edd68dc54d8a7f441656194f1fdf1df7fa87ced
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2025-06-03 14:27:10 +0530

    user wise project report

--- resources/views/reports/userProjectReport.blade.php
+++ resources/views/reports/userProjectReport.blade.php
@@ -104,7 +104,7 @@
                           
                               $subProjectName = $project['prj_id'] != null && $project['sub_prj_id'] != null ? App\Http\Helper\Admin\Helpers::subProjectName($project['prj_id'], $project['sub_prj_id'])['sub_project_name'] : '--'; 
                               $matchKey =array_keys($clientIds,  $project['prj_id']);
-                              dd('clientIds:',$clientIds,'subPrjIds:',$subPrjIds,$matchKey,$subPrjIds[$matchKey],in_array($project['sub_prj_id'], $subPrjIds);
+                              dd('clientIds:',$clientIds,'subPrjIds:',$subPrjIds,$matchKey,$subPrjIds[$matchKey],in_array($project['sub_prj_id'], $subPrjIds));
                              dd($subProjectName !== '--' ,  isset($formConfigurationDetails[$project['sub_prj_id']]),$formConfigurationDetails[$project['sub_prj_id']] == $project['prj_id'],$formConfigurationDetails[$project['sub_prj_id']] , $project['prj_id'],$formConfigurationDetails);
                            @endphp
                            @if($subProjectName !== '--' &&  isset($formConfigurationDetails[$project['sub_prj_id']]) &&  $formConfigurationDetails[$project['sub_prj_id']] == $project['prj_id'])        
