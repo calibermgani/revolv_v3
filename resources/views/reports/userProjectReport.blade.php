7858b23a7ec1df3e8d5246131aa99e60a27903c3
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2025-06-04 10:25:22 +0530

    user Project Report

--- resources/views/reports/userProjectReport.blade.php
+++ resources/views/reports/userProjectReport.blade.php
@@ -148,9 +148,12 @@
                                 @foreach ($projectDetails as $project)
                                     @php                          
                                     $subProjectName = $project['prj_id'] != null && $project['sub_prj_id'] != null ? App\Http\Helper\Admin\Helpers::subProjectName($project['prj_id'], $project['sub_prj_id'])['sub_project_name'] : '--'; 
-                                    $matchKey =array_keys($clientIds, $project['prj_id']);dd($matchKey);
+                                    $matchKey =array_keys($clientIds, $project['prj_id']);
                                     @endphp
-                                    @if($subProjectName !== '--' && !empty($matchKey) && in_array($project['sub_prj_id'], $subPrjIds[$matchKey[0]]))                                            
+                                    @if($subProjectName !== '--' && !empty($matchKey) && in_array($project['sub_prj_id'], $subPrjIds[$matchKey[0]]))    
+                                    @php
+                                    dd($matchKey);
+                                    @endphp                                        
                                         <tr>
                                             <td>{{ $project['emp_id'] }}</td>
                                             <td>{{ $project['user_name'] }}</td>
