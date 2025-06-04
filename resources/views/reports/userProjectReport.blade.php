eca0514351659efcb089584125c90c5413828283
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2025-06-04 15:49:51 +0530

    user Project Report

--- resources/views/reports/userProjectReport.blade.php
+++ resources/views/reports/userProjectReport.blade.php
@@ -181,7 +181,7 @@
                                                         ->whereIn('chart_status', ['CE_Inprocess','CE_Pending','CE_Completed','CE_Clarification','CE_Hold','AR_non_workable','Revoke','QA_Assigned','QA_Inprocess','QA_Pending','QA_Completed','QA_Clarification','QA_Hold'])
                                                         ->count();
                                                     @endphp
-                                                    <td style="background-color: #d9dee9;">{{$resolvCount}}</td> {{-- Resolv Count --}}
+                                                    <td style="background-color: #ebedf3;">{{$resolvCount}}</td> {{-- Resolv Count --}}
                                                     <td>{{ $aimsCount }}</td> {{-- AIMS Count --}}
                                                 @endforeach                               
                                         </tr>
