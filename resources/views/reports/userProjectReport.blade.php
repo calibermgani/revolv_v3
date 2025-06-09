85811586aa4eaa2b9c59f34cb966e0d7d0f92f4b
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2025-06-09 12:25:40 +0530

    user Project Report

--- resources/views/reports/userProjectReport.blade.php
+++ resources/views/reports/userProjectReport.blade.php
@@ -8,12 +8,12 @@
                     <a class="project_header" href="" style="margin-left:-1.7rem">User Project Report</a>
                 </div>
             </div>
-            <div class="card-toolbar d-inline float-right mt-0">
+              <div class="card-toolbar d-inline float-right mt-0">
                 <div class="card-toolbar d-inline float-right mt-3">
                     <div class="outside" href="javascript:void(0);" id="export_data">
-                       <span data-dismiss="modal" data-toggle="tooltip" data-placement="left" data-original-title="Export" style="font-size:13px">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="12" fill="currentColor" class="bi bi-box-arrow-up" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M3.5 6a.5.5 0 0 0-.5.5v8a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5v-8a.5.5 0 0 0-.5-.5h-2a.5.5 0 0 1 0-1h2A1.5 1.5 0 0 1 14 6.5v8a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 14.5v-8A1.5 1.5 0 0 1 3.5 5h2a.5.5 0 0 1 0 1z"/><path fill-rule="evenodd" d="M7.646.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 1.707V10.5a.5.5 0 0 1-1 0V1.707L5.354 3.854a.5.5 0 1 1-.708-.708z"/>
-                                                        </svg>&nbsp;&nbsp;&nbsp;<span>Export</span></span>
+                        <span data-dismiss="modal" class="btn btn-secondary--icon p-2 float-right mr-3 mb-3"
+                            id="export_data" data-toggle="tooltip" data-placement="left" data-original-title="Export" style="background-color: #139AB3;font-size:13px"> <i
+                                class="fas fa-file-excel" style="font-size:14px;color: white"></i> Export</span>
 
                     </div>
                 </div>
