751b3294f775c61844547f2074359f46834ff0f5
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2025-06-09 15:22:29 +0530

    user Project Report

--- app/Exports/userProjectExport.php
+++ app/Exports/userProjectExport.php
@@ -144,11 +144,8 @@ class userProjectExport implements FromCollection, WithHeadings, WithMapping, Wi
                     $paProject = \App\Http\Helper\Admin\Helpers::projectName($project['prj_id']);
                     $decodedClientName = $paProject ? $paProject['project_name'] : null;
 
-                    $decodedSubProjectName = $project['sub_prj_id'] == null
-                        ? 'project'
-                        : ($project['prj_id'] != null
-                            ? (\App\Http\Helper\Admin\Helpers::subProjectName($project['prj_id'], $project['sub_prj_id'])['sub_project_name'] ?? null)
-                            : null);
+                       $decodedSubProjectName = $project['sub_prj_id'] == null ? 'project' :($project['prj_id'] != null ? (\App\Http\Helper\Admin\Helpers::subProjectName($project['prj_id'], $project['sub_prj_id']) != null ? \App\Http\Helper\Admin\Helpers::subProjectName($project['prj_id'], $project['sub_prj_id'])->sub_project_name : null) : null);
+                                                  
 
                     $table_name = Str::slug(Str::lower($decodedClientName) . '_' . Str::lower($decodedSubProjectName), '_');
                     $modelClass = "App\\Models\\" . Str::studly($table_name);
