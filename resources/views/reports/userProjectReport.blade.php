a07fc573e36e251a705333f365a1cbc634d92ad2
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2025-06-25 10:13:01 +0530

    user Project Report

--- resources/views/reports/userProjectReport.blade.php
+++ resources/views/reports/userProjectReport.blade.php
@@ -160,8 +160,12 @@
                         @if (isset($prjDetailsList) && is_array($prjDetailsList))                           
                             @foreach ($prjDetailsList as $projectDetails)
                                 @foreach ($projectDetails as $project)
-                                    @php                          
-                                    $subProjectName = $project['prj_id'] != null && $project['sub_prj_id'] != null ? App\Http\Helper\Admin\Helpers::subProjectName($project['prj_id'], $project['sub_prj_id'])['sub_project_name'] : '--'; 
+                                    @php 
+                                    $subProjectDetails = $project['prj_id'] != null && $project['sub_prj_id'] != null 
+                                        ? App\Http\Helper\Admin\Helpers::subProjectName($project['prj_id'], $project['sub_prj_id']) 
+                                        : null;
+
+                                    $subProjectName = $subProjectDetails['sub_project_name'] ?? '--';                         
                                     $matchKey =array_keys($clientIds, $project['prj_id']);
                                     @endphp
                                     @if($subProjectName !== '--' && !empty($matchKey) && in_array($project['sub_prj_id'], $subPrjIds[$matchKey[0]]))    
