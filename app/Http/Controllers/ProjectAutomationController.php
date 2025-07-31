46223d90026334cdf723c44d0c72c239185058b4
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2025-07-31 11:00:08 +0530

    vein institute duplicate criteria changed

--- app/Http/Controllers/ProjectAutomationController.php
+++ app/Http/Controllers/ProjectAutomationController.php
@@ -4600,7 +4600,7 @@ public function NexTrustBillingArDuplicates(Request $request)
     {
         try {
             $attributes = [
-                'account_no' => isset($request->account_no) && $request->account_no != "NULL" ? $request->account_no : NULL,  
+                'unique_id' => isset($request->unique_id) && $request->unique_id != "NULL" ? $request->unique_id : NULL,  
                 'patient_name' => isset($request->patient_name) && $request->patient_name != "NULL" ? $request->patient_name : NULL,  
                 'dos' => isset($request->dos) && $request->dos != "NULL" ? $request->dos : NULL,  
                 'balance' => isset($request->balance) && $request->balance != "NULL" ? $request->balance : NULL, 
