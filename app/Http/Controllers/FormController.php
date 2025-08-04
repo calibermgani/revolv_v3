53429b0b16b683e23be1cf616226b0367d51a260
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2025-08-04 12:58:12 +0530

    form controller add denial & substatus code

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -544,7 +544,7 @@ class FormController extends Controller
             
             try {
                 DB::beginTransaction();
-                $data = $request->all();dd($data);
+                $data = $request->all();
                 $additionalLabelArray = [
                     "AR Denial Codes",
                     "AR SubStatus Codes"
