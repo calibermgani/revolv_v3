e6050ae9a202f8dc2105b2fcb9c969bc8af20bbd
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2025-08-18 15:48:27 +0530

    touch report

--- app/Http/Controllers/Reports/ReportsController.php
+++ app/Http/Controllers/Reports/ReportsController.php
@@ -1406,7 +1406,7 @@ class ReportsController extends Controller
                         $columnsToExclude = ['invoke_date','CE_emp_id','QA_emp_id','chart_status','ce_hold_reason','qa_hold_reason','qa_work_status','QA_required_sampling','QA_rework_comments','QA_status_code','QA_sub_status_code','qa_classification',
                        'qa_category','qa_scope','QA_followup_date','CE_status_code','CE_sub_status_code','CE_followup_date','updated_at','created_at', 'deleted_at','cpt_trends','icd_trends','modifiers','annex_coder_trends', 'annex_qa_trends', 'qa_cpt_trends', 'qa_icd_trends',
                        'QA_comments_count','coder_work_date','qa_work_date','coder_rework_status','coder_rework_reason','coder_error_count','qa_error_count','tl_error_count','tl_comments','ar_status_code','ar_action_code','ar_manager_rebuttal_status','ar_manager_rebuttal_comments',
-                       'qa_manager_rebuttal_status','ar_substatus_codes','ar_denial_codes','qa_manager_rebuttal_comments'];
+                       'qa_manager_rebuttal_status','ar_substatus_codes','ar_denial_codes','qa_manager_rebuttal_comments','ar_notes','ar_at','qa_at','qa_error_comments','activity','sub_activity','qa_rework_comments'];
                         $columnsHeader = array_filter($columns, function ($column) use ($columnsToExclude) {
                             return !in_array($column, $columnsToExclude);
                         });
@@ -1416,7 +1416,7 @@ class ReportsController extends Controller
                         $columnsToExclude = ['invoke_date','CE_emp_id','QA_emp_id','chart_status','ce_hold_reason','qa_hold_reason','qa_work_status','QA_required_sampling','QA_rework_comments','QA_status_code','QA_sub_status_code','qa_classification',
                              'qa_category','qa_scope','QA_followup_date','CE_status_code','CE_sub_status_code','CE_followup_date','updated_at','created_at', 'deleted_at','cpt_trends','icd_trends','modifiers','annex_coder_trends', 'annex_qa_trends', 'qa_cpt_trends', 'qa_icd_trends',
                              'QA_comments_count','coder_work_date','qa_work_date','coder_rework_status','coder_rework_reason','coder_error_count','qa_error_count','tl_error_count','tl_comments','ar_status_code','ar_action_code','ar_manager_rebuttal_status','ar_manager_rebuttal_comments',
-                             'qa_manager_rebuttal_status','ar_substatus_codes','ar_denial_codes','qa_manager_rebuttal_comments'];
+                             'qa_manager_rebuttal_status','ar_substatus_codes','ar_denial_codes','qa_manager_rebuttal_comments','ar_notes','ar_at','qa_at','qa_error_comments','activity','sub_activity','qa_rework_comments'];
                         $columnsHeader = array_filter($columns, function ($column) use ($columnsToExclude) {
                             return !in_array($column, $columnsToExclude);
                         });
