7b3cadb62386fc6d4f89b18d7377324422b63639
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2025-09-26 15:16:51 +0530

    bulk report export

--- app/Exports/BulkProdcutionExport.php
+++ app/Exports/BulkProdcutionExport.php
@@ -1,7 +1,5 @@
 <?php
 
-<?php
-
 namespace App\Exports;
 
 use Illuminate\Support\Facades\DB;
@@ -18,14 +16,22 @@ class BulkProdcutionExport implements FromGenerator, WithHeadings, WithMapping
     public function __construct(array $requestData, string $tableName, array $columns)
     {
         $this->requestData = $requestData;
-        $this->tableName = $tableName;
-        $this->columns = $columns;
+        $this->tableName   = $tableName;
+        $this->columns     = $columns;
     }
 
-    public function generator()
+    /**
+     * Stream rows using a generator to avoid memory issues
+     */
+    public function generator(): \Generator
     {
         $query = DB::table($this->tableName)
-            ->join('caller_charts_work_logs', 'caller_charts_work_logs.record_id', '=', $this->tableName.'.parent_id')
+            ->join(
+                'caller_charts_work_logs',
+                'caller_charts_work_logs.record_id',
+                '=',
+                $this->tableName . '.parent_id'
+            )
             ->select($this->columns)
             ->where('caller_charts_work_logs.project_id', $this->requestData['project_id'])
             ->where('caller_charts_work_logs.sub_project_id', $this->requestData['sub_project_id'])
@@ -33,16 +39,16 @@ class BulkProdcutionExport implements FromGenerator, WithHeadings, WithMapping
 
         if (!empty($this->requestData['work_date'])) {
             $work_date = explode(' - ', $this->requestData['work_date']);
-            $query->whereBetween('caller_charts_work_logs.start_time', [
-                date('Y-m-d 08:00:00', strtotime($work_date[0])),
-                date('Y-m-d 07:59:00', strtotime($work_date[1].' +1 day'))
-            ]);
+            $start_date = date('Y-m-d 08:00:00', strtotime($work_date[0]));
+            $end_date   = date('Y-m-d 07:59:00', strtotime($work_date[1] . ' +1 day'));
+            $query->whereBetween('caller_charts_work_logs.start_time', [$start_date, $end_date]);
         }
 
         if (!empty($this->requestData['user'])) {
-            $query->where(function($q) {
-                $q->where('CE_emp_id', $this->requestData['user'])
-                  ->orWhere('QA_emp_id', $this->requestData['user']);
+            $user = $this->requestData['user'];
+            $query->where(function ($q) use ($user) {
+                $q->where('CE_emp_id', $user)
+                  ->orWhere('QA_emp_id', $user);
             });
         }
 
@@ -50,7 +56,6 @@ class BulkProdcutionExport implements FromGenerator, WithHeadings, WithMapping
             $query->where('caller_charts_work_logs.record_status', $this->requestData['client_status']);
         }
 
-        // Use cursor() for memory-efficient streaming
         foreach ($query->cursor() as $row) {
             yield $row;
         }
