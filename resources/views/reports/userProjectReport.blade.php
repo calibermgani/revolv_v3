2830bf6ccc035440ba78f305bc711a21b01e10a5
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2025-09-26 12:43:26 +0530

    app3

--- resources/views/reports/userProjectReport.blade.php
+++ resources/views/reports/userProjectReport.blade.php
@@ -351,16 +351,39 @@
                     $('#sub_project_list').val('').trigger('change.select2');
                        // isResetting = false;
                 });
-                    $(document).on('click', '#export_data', function() {
+                    // $(document).on('click', '#export_data', function() {
+
+                    //     var query = {
+                    //         work_date: $('#work_date').val(),
+                    //         project_id: $('#project_id').val(),
+                    //         sub_project_id: $('#sub_project_list').val(),
+                    //         user_name: $('#manager_name').val()
+                    //     }
+                    //     window.location.href = "{{ URL::to('report/user_project_report_export') }}?" + $.param(query)
+                    // });
+                    $(document).on('click', '#export_data', function () {
+                        // show loader
+                        $('#global-loader').css({ display: 'flex', opacity: 1 });
 
                         var query = {
                             work_date: $('#work_date').val(),
                             project_id: $('#project_id').val(),
                             sub_project_id: $('#sub_project_list').val(),
                             user_name: $('#manager_name').val()
-                        }
-                        window.location.href = "{{ URL::to('report/user_project_report_export') }}?" + $.param(query)
+                        };
+
+                        // force HTTPS
+                        var exportUrl = "{{ secure_url('report/user_project_report_export') }}?" + $.param(query);
+
+                        // trigger download
+                        window.location.href = exportUrl;
+
+                        // hide loader after 5s (download prompt already triggered)
+                        setTimeout(() => {
+                            $('#global-loader').fadeOut(300);
+                        }, 5000);
                     });
+
 
                 $(document).on('click', '#form_submit', function(e) {
                     e.preventDefault();
