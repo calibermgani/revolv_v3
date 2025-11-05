82d8f6b89e4e5ebe06296c1a4071d80f08ca083d
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2025-11-05 13:12:40 +0530

    prodcution type added

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -641,19 +641,19 @@ class FormController extends Controller
                         $requiredData['project_type'] = $data['project_type_val'];
                         $requiredData['claim_type'] = $data['claim_type_val'];
                         $existingRecord->update($requiredData);
-                        //  $columnName = Str::lower(str_replace([' ', '/'], ['_', '_else_'], $data['label_name'][$i]));
-                        // if ($data['input_type'][$i] == 'text' || $data['input_type'][$i] == 'date_range') {
-                        //     $columns[$columnName] = 'TEXT';
-                        // } else if ($data['input_type'][$i] == 'select' || $data['input_type'][$i] == 'checkbox' || $data['input_type'][$i] == 'radio') {                          
-                        //         $columns[$columnName] = "TEXT";                          
+                         $columnName = Str::lower(str_replace([' ', '/'], ['_', '_else_'], $data['label_name'][$i]));
+                        if ($data['input_type'][$i] == 'text' || $data['input_type'][$i] == 'date_range') {
+                            $columns[$columnName] = 'TEXT';
+                        } else if ($data['input_type'][$i] == 'select' || $data['input_type'][$i] == 'checkbox' || $data['input_type'][$i] == 'radio') {                          
+                                $columns[$columnName] = "TEXT";                          
                            
-                        // } else if ($data['input_type'][$i] == 'date') {
-                        //     $columns[$columnName] = 'DATE';
-                        // } else if ($data['input_type'][$i] == 'textarea') {
-                        //     $columns[$columnName] = 'TEXT';
-                        // } else if ($data['input_type'][$i] == 'datetime' ) {
-                        //     $columns[$columnName] = 'DATETIME';
-                        // }                                              
+                        } else if ($data['input_type'][$i] == 'date') {
+                            $columns[$columnName] = 'DATE';
+                        } else if ($data['input_type'][$i] == 'textarea') {
+                            $columns[$columnName] = 'TEXT';
+                        } else if ($data['input_type'][$i] == 'datetime' ) {
+                            $columns[$columnName] = 'DATETIME';
+                        }                                              
                     } else {
                         $requiredData['project_id'] = $data['project_id_val'];
                         $requiredData['sub_project_id'] = $data['sub_project_id_val'] != null ? $data['sub_project_id_val'] : NULL;
