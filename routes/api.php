88a3c39af83b16ca50a66e07d8a9d881de6bc5c0
vijayalaxmi-caliber <vijayalaxmi@caliberfocus.com>
2026-04-07 15:11:44 +0530

    Dickson

--- routes/api.php
+++ routes/api.php
@@ -224,8 +224,8 @@ Route::group(['prefix' => 'projects'], function() {
     Route::any('twh_ob_twhfollow_up_duplicate', 'App\Http\Controllers\ProjectAuthAutomationController@twhobTwhFollowUpDuplicates');
     Route::any('dma_dickson_epic_labs_denials_follow_up', 'App\Http\Controllers\ProjectAuthAutomationController@dmaDicksonepiclabsDenialsFollowUp');
     Route::any('dma_dickson_epic_labs_denials_follow_up_duplicate', 'App\Http\Controllers\ProjectAuthAutomationController@dmaDicksonepiclabsDenialsFollowUpDuplicates');
-    Route::any('dma_dickson_epic_noRrsponse_follow_up', 'App\Http\Controllers\ProjectAuthAutomationController@dmaDicksonepicnoResponseFollowUp');
-    Route::any('dma_dickson_epic_noRrsponse_follow_up_duplicate', 'App\Http\Controllers\ProjectAuthAutomationController@dmaDicksonepicnoResponseFollowUpDuplicates');
+    Route::any('dma_dickson_epic_noresponse_follow_up', 'App\Http\Controllers\ProjectAuthAutomationController@dmaDicksonepicnoResponseFollowUp');
+    Route::any('dma_dickson_epic_noresponse_follow_up_duplicate', 'App\Http\Controllers\ProjectAuthAutomationController@dmaDicksonepicnoResponseFollowUpDuplicates');
     Route::any('get_project_columns', 'App\Http\Controllers\FormController@getProjectColumns');
 
 
